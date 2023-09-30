b89191359145e4704f3d49d94972d9928957669e
Another <89122911+hexvel@users.noreply.github.com>
2023-09-30 15:59:25 +0300

    Update AuthPage.jsx

--- src/components/AuthPage.jsx
+++ src/components/AuthPage.jsx
@@ -5,7 +5,7 @@ const AuthPage = (props) => {
     e.preventDefault();
     const { value } = e.target[0];
     axios
-      .post("http://localhost:3000/auth", {
+      .post("https://web-chat-backend-i1xj.onrender.com/auth", {
         username: value,
       })
       .then((res) => props.onAuth({ ...res.data, secret: value }))
